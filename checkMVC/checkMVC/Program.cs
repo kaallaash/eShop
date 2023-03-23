808921664dbbb45c614c926a44bdd494b33fbf14
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-03-23 04:25:00 +0300

    add mvc architecture

--- checkMVC/checkMVC/Program.cs
+++ checkMVC/checkMVC/Program.cs
@@ -1,20 +1,14 @@
-using eShop.BLL.DI;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
-//builder.Services.AddRazorPages();
-
-builder.Services.AddBll(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -26,7 +20,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-//app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
