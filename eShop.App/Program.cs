0005e60489d26992b800f0258e90c824f0776967
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-03-24 03:39:08 +0300

    refactoring program.cs

--- eShop.App/Program.cs
+++ eShop.App/Program.cs
@@ -3,21 +3,16 @@ using eShop.BLL.DI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllersWithViews();
-//builder.Services.AddRazorPages();
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddBll(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -28,7 +23,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-//app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Product}/{action=Index}/{id?}");
