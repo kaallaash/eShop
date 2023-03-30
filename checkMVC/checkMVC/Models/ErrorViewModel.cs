3d307135eb1a2c1f74f39e483d61c409f38f4ce6
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-03-30 03:58:54 +0300

    delete checkMVC solution

--- checkMVC/checkMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace checkMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
