88e48770a02f4b5b18abc5931efcf84a8b530ab9
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-05-04 03:10:12 +0300

    add UserViewModels

---
+++ eShop.App/ViewModels/User/UserDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Core.Enums;
+
+namespace eShop.App.ViewModels.User;
+
+public class UserDetailsViewModel
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public Role Role { get; set; }
+}
---
+++ eShop.App/ViewModels/User/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace eShop.App.ViewModels.User;
+
+public class UserViewModel
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
