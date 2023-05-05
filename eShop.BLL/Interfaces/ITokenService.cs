1bb705658f9f9e57007b2ab2499540b3db4d3119
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-05-06 01:23:33 +0300

    add async to method names

--- eShop.BLL/Interfaces/ITokenService.cs
+++ eShop.BLL/Interfaces/ITokenService.cs
@@ -1,11 +1,10 @@
 ﻿using eShop.BLL.Models;
 using eShop.BLL.Models.Token;
-using System.Runtime.CompilerServices;
 
 namespace eShop.BLL.Interfaces;
 
 public interface ITokenService
 {
-    Task<TokenDetailsModel?> Login(LoginBllModel login, CancellationToken cancellationToken);
-    Task<TokenDetailsModel?> RefreshToken(TokenModel tokenModel, CancellationToken cancellationToken);
+    Task<TokenDetailsModel?> LoginAsync(LoginBllModel login, CancellationToken cancellationToken);
+    Task<TokenDetailsModel?> RefreshTokenAsync(TokenModel tokenModel, CancellationToken cancellationToken);
 }
