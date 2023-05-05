9d0abd61480f64a4384d1ac7df29adab8ae61d81
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-05-06 01:19:16 +0300

    add ITokenService

---
+++ eShop.BLL/Interfaces/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using eShop.BLL.Models;
+using eShop.BLL.Models.Token;
+using System.Runtime.CompilerServices;
+
+namespace eShop.BLL.Interfaces;
+
+public interface ITokenService
+{
+    Task<TokenDetailsModel?> Login(LoginBllModel login, CancellationToken cancellationToken);
+    Task<TokenDetailsModel?> RefreshToken(TokenModel tokenModel, CancellationToken cancellationToken);
+}
