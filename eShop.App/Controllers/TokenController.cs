ffa9f51d3cd0a2f37b066c8780af9d0e738ab01c
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-05-06 01:58:06 +0300

    refactoring TokenController

--- eShop.App/Controllers/TokenController.cs
+++ eShop.App/Controllers/TokenController.cs
@@ -1,14 +1,11 @@
 ﻿using AutoMapper;
 using eShop.BLL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text;
-using eShop.App.Models;
 using eShop.BLL.Models;
 using eShop.App.ViewModels.User;
+using eShop.BLL.Models.Token;
+using eShop.App.Models.Token;
 
 namespace eShop.App.Controllers;
 
@@ -16,147 +13,39 @@ namespace eShop.App.Controllers;
 [ApiController]
 public class TokenController : Controller
 {
-    private readonly IConfiguration _configuration;
-    private readonly IUserService _userService;
+    private readonly ITokenService _tokenService;
     private readonly IMapper _mapper;
 
-    public TokenController(IConfiguration config, IUserService userService, IMapper mapper)
+    public TokenController(ITokenService tokenService, IMapper mapper)
     {
-        _configuration = config;
-        _userService = userService;
+        _tokenService = tokenService;
         _mapper = mapper;
     }
 
     [HttpPost]
     public async Task<IActionResult> Login(LoginViewModel loginModel, CancellationToken cancellationToken)
     {
-        if (string.IsNullOrEmpty(loginModel.Username) ||
-            string.IsNullOrEmpty(loginModel.Password))
+        var token = await _tokenService.LoginAsync(_mapper.Map<LoginBllModel>(loginModel), cancellationToken);
+
+        if (token is null)
         {
             return BadRequest();
         }
 
-        var user = await _userService.GetByLoginAsync(_mapper.Map<LoginBllModel>(loginModel), cancellationToken);
-
-        if (user?.Username is null || user?.Role is null)
-        {
-            return BadRequest("Invalid credentials");
-        }
-
-        var claims = new[] {
-            new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
-            new Claim(ClaimTypes.Name, user.Username),
-            new Claim(ClaimTypes.Role, user.Role.ToString()),
-        };
-
-        var accessToken = CreateToken(claims);
-        var newRefreshToken = GenerateRefreshToken();
-        _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInDays"], out var refreshTokenValidityInDays);
-
-        user.RefreshToken = newRefreshToken;
-        user.RefreshTokenExpiryTime = DateTime.Now.AddDays(refreshTokenValidityInDays);
-
-        await _userService.UpdateAsync(user, cancellationToken);
-
-        return Ok(
-            new
-            {
-                AccessToken = new JwtSecurityTokenHandler().WriteToken(accessToken),
-                RefreshToken = newRefreshToken,
-                ExpiryTime = user.RefreshTokenExpiryTime
-            });
+        return Ok(token);
     }
 
     [HttpPost]
     [Route("refresh-token")]
     public async Task<IActionResult> RefreshToken(TokenAppModel tokenModel, CancellationToken cancellationToken)
     {
-        var accessToken = tokenModel.AccessToken;
-        var refreshToken = tokenModel.RefreshToken;
+        var token = await _tokenService.RefreshTokenAsync(_mapper.Map<TokenModel>(tokenModel), cancellationToken);
 
-        var principal = GetPrincipalFromExpiredToken(accessToken);
-
-        if (principal?.Identity?.Name is null)
+        if (token is null)
         {
-            return BadRequest("Invalid access token or refresh token");
+            return BadRequest();
         }
 
-        var userName = principal.Identity.Name;
-
-        var user = await _userService.GetByUsernameAsync(userName, cancellationToken);
-
-        if (user is null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime >= DateTime.Now)
-        {
-            return BadRequest("Invalid access token or refresh token");
-        }
-
-        var newAccessToken = CreateToken(principal.Claims.ToList());
-        var newRefreshToken = GenerateRefreshToken();
-
-        _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInDays"], out var refreshTokenValidityInDays);
-
-        user.RefreshToken = newRefreshToken;
-        user.RefreshTokenExpiryTime = DateTime.Now.AddDays(refreshTokenValidityInDays);
-        
-        await _userService.UpdateAsync(user, cancellationToken);
-
-        return Ok(
-        new
-        {
-            AccessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
-            RefreshToken = newRefreshToken,
-            ExpiryTime = user.RefreshTokenExpiryTime
-        });
-    }
-
-    private ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token)
-    {
-        var tokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateAudience = false,
-            ValidateIssuer = false,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"])),
-            ValidateLifetime = false
-        };
-
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
-        if (securityToken is not JwtSecurityToken jwtSecurityToken
-            || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
-            throw new SecurityTokenException("Invalid token");
-
-        return principal;
-
-    }
-
-    private SecurityToken CreateToken(IEnumerable<Claim> authClaims)
-    {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-        _ = int.TryParse(_configuration["JWT:TokenValidityInMinutes"], out var tokenValidityInMinutes);
-
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Issuer = _configuration["JWT:Issuer"],
-            Audience = _configuration["JWT:Audience"],
-            Subject = new ClaimsIdentity(authClaims),
-            Expires = DateTime.Now.AddMinutes(tokenValidityInMinutes),
-            SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
-        };
-
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-
-        return token;
-    }
-
-    private static string GenerateRefreshToken()
-    {
-        var randomNumber = new byte[64];
-        using var rng = RandomNumberGenerator.Create();
-        rng.GetBytes(randomNumber);
-        return Convert.ToBase64String(randomNumber);
+        return Ok(token);
     }
 }
--- eShop.App/Models/TokenAppModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace eShop.App.Models;
-
-public class TokenAppModel
-{
-    public string? AccessToken { get; set; }
-    public string? RefreshToken { get; set; }
-}
